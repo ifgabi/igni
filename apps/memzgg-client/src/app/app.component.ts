d1cbfbb6fadd3b0a851b61bf9b67ae035b02892b
dizicode <gabriel_m20032003@yahoo.com>
2022-01-27 01:10:20 -0500

    initial commit

---
+++ apps/memzgg-client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'memzgg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'memzgg-client';
+}
